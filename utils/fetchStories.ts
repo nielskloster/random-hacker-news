c46a0c98d424028ac698f1a97972ae7356e22c1d
Niels Kloster Andreassen <niels@bestbrains.dk>
2023-02-23 22:13:36 +0100

    Fetch actual titles

--- utils/fetchStories.ts
+++ utils/fetchStories.ts
@@ -6,8 +6,11 @@ const paths = {
   AuthorInfo: (id: number) => `https://hacker-news.firebaseio.com/v0/user/${id}.json`,
 } as const
 
+type StoryInfo = {
+  title: string
+}
 
-export async function fetchStoryIds(): Promise<number[]> {
+async function fetchStoryIds(): Promise<number[]> {
   const response = await fetch(paths.TopStories())
   if (!response.ok) {
     throw new Error('Could not fetch stories')
@@ -16,11 +19,23 @@ export async function fetchStoryIds(): Promise<number[]> {
   return data
 }
 
+async function fetchStoryInfo(id: number): Promise<Story> {
+  const response = await fetch(paths.StoryInfo(id))
+  if (!response.ok) {
+    throw new Error('Could not fetch story info for story: ' + id)
+  }
+  const data: StoryInfo = await response.json()
+  return { id, title: data.title }
+}
+
 export async function fetchStories(): Promise<Story[]> {
   const storyIds = await fetchStoryIds()
 
-  return storyIds
+  const randomIds = storyIds
     .sort(() => Math.random() - 0.5)
     .slice(0, 10)
-    .map(id => ({ id, title: id.toString() }))
+
+  const stories = await Promise.all(randomIds.map(id => fetchStoryInfo(id)))
+
+  return stories
 }
