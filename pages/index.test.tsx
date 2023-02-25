55db57501da9eb41852b934d6d78f656458248eb
Niels Kloster Andreassen <niels@bestbrains.dk>
2023-02-25 13:03:36 +0100

    Setup MSW

--- pages/index.test.tsx
+++ pages/index.test.tsx
@@ -3,10 +3,20 @@ import { render, waitForElementToBeRemoved, screen } from '@testing-library/reac
 import IndexPage from '.'
 import { setupServer } from 'msw/node'
 import { rest } from 'msw'
+import { paths } from '../utils/paths'
+import { fetch } from 'cross-fetch'
+
+global.fetch = fetch
 
 export const restHandlers = [
-  rest.get('https://hacker-news.firebaseio.com/v0/topstories.json', (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json([]))
+  rest.get(paths.TopStories(), (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json([1]))
+  }),
+  rest.get(paths.StoryInfo(1), (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({ by: "person" }))
+  }),
+  rest.get(paths.AuthorInfo("person"), (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({}))
   }),
 ]
 
