da6130237ad9686fcc3a278302bc184405999467
Niels Kloster Andreassen <niels@bestbrains.dk>
2023-02-25 12:53:38 +0100

    Setup basic snapshot test

--- pages/index.test.tsx
+++ pages/index.test.tsx
@@ -1,10 +1,26 @@
-import { describe, test, expect } from 'vitest'
-import { render, screen } from '@testing-library/react'
+import { describe, test, expect, afterAll, afterEach, beforeAll } from 'vitest'
+import { render, waitForElementToBeRemoved, screen } from '@testing-library/react'
 import IndexPage from '.'
+import { setupServer } from 'msw/node'
+import { rest } from 'msw'
+
+export const restHandlers = [
+  rest.get('https://hacker-news.firebaseio.com/v0/topstories.json', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json([]))
+  }),
+]
+
+const server = setupServer(...restHandlers)
 
 describe('Index page', () => {
-  test('App mounts properly', () => {
+  beforeAll(() => server.listen({ onUnhandledRequest: 'warn' }))
+  afterAll(() => server.close())
+  afterEach(() => server.resetHandlers())
+
+  test('App mounts properly', async () => {
     const wrapper = render(<IndexPage />)
-    expect(wrapper).toBeTruthy()
+    await waitForElementToBeRemoved(() => screen.queryByLabelText('loading'))
+
+    expect(wrapper).toMatchSnapshot()
   })
 })
