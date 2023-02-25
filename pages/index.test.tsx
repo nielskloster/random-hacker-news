b2fa961a3732a3d712ef5c0771e9a159d12680af
Niels Kloster Andreassen <niels@bestbrains.dk>
2023-02-25 12:53:38 +0100

    Setup basic snapshot test

--- pages/index.test.tsx
+++ pages/index.test.tsx
@@ -1,10 +1,11 @@
 import { describe, test, expect } from 'vitest'
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import IndexPage from '.'
 
 describe('Index page', () => {
-  test('App mounts properly', () => {
+  test('App mounts properly', async () => {
     const wrapper = render(<IndexPage />)
-    expect(wrapper).toBeTruthy()
+
+    expect(wrapper).toMatchSnapshot()
   })
 })
